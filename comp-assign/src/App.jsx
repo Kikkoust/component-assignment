5452429e9b289e5231bbd7adc962e785befd33d0
Kikkoust <156084871+Kikkoust@users.noreply.github.com>
2024-11-05 20:29:48 +0200

    function SelectProduct lisätty ja sen sisään divit
    Functioon SelectProduct lisätty ja sen sisään eri divejä. Lisäksi index.css joitain pieniä tyylejä lisätty sivulle (background ja header).

--- comp-assign/src/App.jsx
+++ comp-assign/src/App.jsx
@@ -3,21 +3,45 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 
 
+
+
 function App() {
 
 
   return (
     <div>
       <Header/>
+      <SelectProduct/>
     </div>
   )
 }
 
 function Header(){
   return(
-    <div>
+    <div className="header">
       <h1>Welcome to product page!</h1>
     </div>
+  )
+}
+
+function SelectProduct(){
+
+  return(
+    <><div className="product">
+      <h2>Select product</h2>
+    </div>
+    
+    <div id="name">
+      <p>Product:</p>
+
+    </div>
+
+    <div id="quantity">
+      <p>Quauntity:</p>
+    
+    </div>
+    
+  </>
   )
 }
 
